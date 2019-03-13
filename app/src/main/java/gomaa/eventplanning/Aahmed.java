7b9af18be0ed7383cc6afa60cde950488c4c0209
moataz <moatazhatem7@gmail.com>
2019-03-13 15:29:34 +0200

    tzzzzaaaaaa

--- app/src/main/java/gomaa/eventplanning/Aahmed.java
+++ app/src/main/java/gomaa/eventplanning/Aahmed.java
@@ -1,4 +1,38 @@
 package gomaa.eventplanning;
 
-public class Aahmed {
+public class Aahmed
+{
+    private String Title;
+    private String Category;
+    private String Description;
+
+    public String getTitle()
+    {
+        return Title;
+    }
+
+    public void setTitle(String title)
+    {
+        Title = title;
+    }
+
+    public String getCategory()
+    {
+        return Category;
+    }
+
+    public void setCategory(String category)
+    {
+        Category = category;
+    }
+
+    public String getDescription()
+    {
+        return Description;
+    }
+
+    public void setDescription(String description)
+    {
+        Description = description;
+    }
 }
