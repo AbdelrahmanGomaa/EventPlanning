b2a9d34e5103a39b417425dcd2bb27cf0499cca5
moataz <moatazhatem7@gmail.com>
2019-03-13 15:49:44 +0200

    ghghghghghghghghghghgh3

--- app/src/main/java/gomaa/eventplanning/ATzaNewwwwbitchassHole.java
+++ app/src/main/java/gomaa/eventplanning/ATzaNewwwwbitchassHole.java
@@ -1,4 +1,11 @@
 package gomaa.eventplanning;
 
 public class ATzaNewwwwbitchassHole {
+    private String Title;
+    private String Category;
+    private String Description;
+
+    private String Title4;
+    private String Category4;
+    private String Description4;
 }
